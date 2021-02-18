cc4ee5c8a6fa9809ba0aa766f5158c1f8ab13cdf
Mário Santos Sousa <m_santos_sousa@hotmail.com>
2021-02-18 11:44:03 +0000

    Update MarkupBuilder.groovy

--- src/builders/MarkupBuilder.groovy
+++ src/builders/MarkupBuilder.groovy
@@ -3,6 +3,7 @@ import groovy.xml.MarkupBuilder
 MarkupBuilder builder = new MarkupBuilder()
 builder.omitNullAttributes = true
 
+//xml
 builder.sports {
     sport(id:1){
         name 'Baseball'
@@ -14,3 +15,57 @@ builder.sports {
         name ''
     }    
 }
+
+//html
+def languages = [
+    [id:1, name:"java"],
+    [id:2, name:"typescript"],
+    [id:3, name:"groovy"]
+]
+
+println "html page"
+builder.html {
+    head {
+        title "About me"
+    }
+    body {
+        h1 "Programming languages"
+        table {
+            tr {
+                th "id"
+                th "name"
+            }
+            tr {
+                languages.each {
+                    td "$it.id"
+                    td "$it.name"
+                }
+            }
+        }
+    }
+}
+
+/*
+<html>
+  <head>
+    <title>About me</title>
+  </head>
+  <body>
+    <h1>Programming languages</h1>
+    <table>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>java</td>
+        <td>2</td>
+        <td>typescript</td>
+        <td>3</td>
+        <td>groovy</td>
+      </tr>
+    </table>
+  </body>
+</html> 
+*/
